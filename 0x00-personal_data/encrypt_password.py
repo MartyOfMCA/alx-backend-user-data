b5af2c47da961e484b25b0ec8d2b6f06007776de
Martin <martyofmca@gmail.com>
2024-02-08 23:06:53 +0000

    validate a password against it's hashed password - 0x00

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,3 +18,23 @@ def hash_password(password: str) -> bytes:
         A salted hashed password.
     """
     return (bcrypt.hashpw(bytes(password, "utf-8"), bcrypt.gensalt(14)))
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks whether the given password is
+    the same as the hashed pasword.
+
+    Parameters:
+        hashed_password : bytes
+        A salted and hashed password.
+
+        password : str
+        The given password to validate.
+
+    Returns:
+        True when both `hashed_password` and
+        `password` are the same otherwise
+        False.
+    """
+    return (bcrypt.checkpw(bytes(password, "utf-8"), hashed_password))
