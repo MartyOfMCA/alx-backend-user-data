b1cb2c745ec6b6f551bcdcebf3130bd206f635e7
Martin <martyofmca@gmail.com>
2024-02-14 14:51:22 +0000

    bind current user to http request - 0x02

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -68,8 +68,11 @@ def before_request():
         if (not auth.current_user(request)):
             abort(403)
 
+        request.current_user = auth.current_user(request)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    #app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
