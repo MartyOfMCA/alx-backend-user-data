d0adefef5475fb23c00a909965326d517849bf9c
Martin <martyofmca@gmail.com>
2024-02-15 02:02:16 +0000

    chore: add code comments - 0x02

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -27,16 +27,20 @@ def login():
     email = request.form.get("email")
     password = request.form.get("password")
 
+    # Check for valid user email
     if (not email or email == ""):
         return (jsonify({"error": "email missing"}), 400)
 
+    # Check for valid passwords
     if (not password or password == ""):
         return (jsonify({"error": "password missing"}), 400)
 
+    # Verify user's email address
     list_of_users = User.search({"email": email})
     if (len(list_of_users) == 0):
         return (jsonify({"error": "no user found for this email"}), 404)
 
+    # Verify user's password
     user = list_of_users[0]
     if (not user.is_valid_password(password)):
         return (jsonify({"error": "wrong password"}), 401)
@@ -44,6 +48,8 @@ def login():
     from api.v1.app import auth
     new_session_id = auth.create_session(user.id)
 
+    # Obtain response object so session
+    # cookie can be added to the sponse
     response = jsonify(user.to_json())
     response.set_cookie(getenv("SESSION_NAME"), new_session_id)
 
