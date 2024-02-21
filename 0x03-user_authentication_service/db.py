a9f2a3a249cc3da7f0d4ec7abe48e1146ca16d96
Martin <martyofmca@gmail.com>
2024-02-21 00:57:00 +0000

    refactor: remove sqlalchemy verbose mode - 0x03

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -22,7 +22,7 @@ class DB:
         """
         Object constructor.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
