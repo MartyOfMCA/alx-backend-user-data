284ee581417947f5e19bc7a35803c9afc1ee1be2
Martin <martyofmca@gmail.com>
2024-02-22 03:22:24 +0000

    register new user - 0x03

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,8 +7,10 @@ from bcrypt import (
         hashpw,
         gensalt
         )
+from sqlalchemy.exc import NoResultFound
 
 from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -33,3 +35,26 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+
+    def register_user(self, email, password) -> User:
+        """
+        Add a new user wih the given
+        credentials.
+
+        Parameters:
+            email : str
+            The email of the new user.
+
+            password : str
+            The password of the new user.
+
+        Return:
+            The new user instance created.
+        """
+        try:
+            # Find existing user with the given
+            # email to abort operation.
+            user = self._db.find_user_by(**{"email": email})
+            print(f"User {email} already exists")
+        except NoResultFound:
+            return (self._db.add_user(email, _hash_password(password)))
