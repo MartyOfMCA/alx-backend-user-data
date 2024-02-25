698eec03450e0717b542d4289ff4080a343b455b
Martin <martyofmca@gmail.com>
2024-02-25 23:50:03 +0000

    raise excetion for already existing records - 0x03

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,6 +56,6 @@ class Auth:
             # Find existing user with the given
             # email to abort operation.
             user = self._db.find_user_by(**{"email": email})
-            print(f"User {email} already exists")
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return (self._db.add_user(email, _hash_password(password)))
