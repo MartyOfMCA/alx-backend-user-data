72e1eaa890b556afae993b3af2edb3025202e16d
Martin <martyofmca@gmail.com>
2024-03-09 00:03:46 +0000

    define endpoint to register users - 0x03

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,11 +5,15 @@ requests to the index route.
 """
 from flask import (
         Flask,
-        jsonify
+        jsonify,
+        request
         )
+
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route(
@@ -21,6 +25,25 @@ def index():
     Handle request to the index endpoint.
     """
     return (jsonify({"message": "Bienvenue"}))
+
+
+@app.route(
+        "/users",
+        methods=["POST"],
+        strict_slashes=False
+        )
+def register():
+    """
+    Handle requests to register a new user.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user_created = AUTH.register_user(email, password)
+        return (jsonify({"email": email, "message": "user created"}))
+    except ValueError:
+        return (jsonify({"message": "email already registered"}), 400)
 
 
 if __name__ == "__main__":
